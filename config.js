b01fb5b64d85b304a2817cf49935aadb66d307bf
dsawyer13 <dsawyer@tutanota.com>
2019-04-24 14:02:15 -0400

    fixed vulnerabilities

--- config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/workoutPalDev';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/workout-pal-test';
-exports.PORT = process.env.PORT || 8080;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongolab-round-33155';
