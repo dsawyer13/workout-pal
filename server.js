b6abadd0655469280532dfa55eb3ab33b927b68d
dsawyer13 <dsawyer@tutanota.com>
2019-04-25 01:45:50 -0400

    fixed typo in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { router: workoutsRouter } = require("./workouts/router");
 
 const app = express();
 
-app.use(bodyPaser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
