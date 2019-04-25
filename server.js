854034be9282a76d1bdae154946619398b0bbb86
dsawyer13 <dsawyer@tutanota.com>
2019-04-25 18:26:11 -0400

    changed proxy value

--- server.js
+++ server.js
@@ -6,21 +6,19 @@ const path = require("path");
 
 const { PORT } = require("./config");
 
-
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
     return res.send(204);
   }
   next();
 });
 
 const { router: workoutsRouter } = require("./workouts/router");
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -34,11 +32,14 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/api/workouts", workoutsRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-})
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password123@ds147436.mlab.com:47436/heroku_x8kjmqlp")
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://user:password123@ds147436.mlab.com:47436/heroku_x8kjmqlp"
+);
 
 app.listen(PORT, function() {
-  console.log(`API listening on port ${PORT}!`)
-})
+  console.log(`API listening on port ${PORT}!`);
+});
